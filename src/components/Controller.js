80cf29153629628eab0ed5f0958bfe51d39a3252
rCheung <rcheung1997@gmail.com>
2024-06-27 04:51:18 -0700

    initialized controller component

---
+++ src/components/Controller.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// sidebar to adjust the properties of current media card
+
+function Controller() {
+  return (
+    <div className="controller">
+
+    </div>
+  )
+}
+
+export default Controller;
