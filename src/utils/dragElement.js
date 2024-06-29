4edd83ec79214c81fc4b23489f66c74fe99e18d9
rCheung <rcheung1997@gmail.com>
2024-06-29 05:55:33 -0700

    cleaned up code

--- src/utils/dragElement.js
+++ src/utils/dragElement.js
@@ -1,16 +1,10 @@
 // source: https://www.w3schools.com/howto/howto_js_draggable.asp
 
-function dragElement(elmnt) {
+function dragElement(element) {
 
   let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-  if (document.getElementById(elmnt.id)) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+  element.onmousedown = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -32,8 +26,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
