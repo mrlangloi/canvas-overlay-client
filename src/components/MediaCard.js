beeedf96d489ee569fb4210df3c887c2f2acefc4
rCheung <rcheung1997@gmail.com>
2024-06-29 05:56:01 -0700

    implemented drag function

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,24 +1,72 @@
-import React, { useEffect, useRef, useState } from 'react';
-import dragElement from '../utils/dragElement';
+import React, { useEffect, useRef } from 'react';
+
 
 // every image/text/gif card on the page
 
-function MediaCard() {
+function MediaCard(props) {
 
-  const [imgURL, setImgURL] = useState("https://via.placeholder.com/150");
-  const [text, setText] = useState("placeholder text");
+  const { id, setActiveElement, setPosX, setPosY, rotation } = props;
+
+  const src = "https://via.placeholder.com/150";
+  const text = "placeholder text";
   const dragRef = useRef(null);
+
+  function dragElement(element) {
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+    element.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";
+      setPosX(element.offsetLeft - pos1);
+      setPosY(element.offsetTop - pos2);
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
 
   useEffect(() => {
     if (dragRef.current) {
       dragElement(dragRef.current);
+      setActiveElement(id);
     }
   }, []);
 
   return (
-    <div className="media-card" ref={dragRef}>
+    <div 
+      className="media-card" 
+      ref={dragRef} 
+      style={{top: 0 + "px", left: 0 + "px", transform: `rotate(${rotation}deg)`}}
+      onMouseDown={() => {setActiveElement(id); console.log(id)}}
+    >
       <p>{text}</p>
-      <img src={imgURL} alt="placeholder" />
+      <img src={src} alt="placeholder" />
       <p>{text}</p>
     </div>
   )
