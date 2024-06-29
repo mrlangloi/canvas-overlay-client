795b48d2a3d2e5a7bf2563b004f02243356d8a34
rCheung <rcheung1997@gmail.com>
2024-06-29 05:54:33 -0700

    implemented some handles

---
+++ src/components/MediaControls.js
@@ -0,0 +1,84 @@
+
+// settings to fine-tune the properties of the media card
+
+function MediaControls(props) {
+
+  const { 
+    activeElement,
+    posX,
+    setPosX, 
+    posY,
+    setPosY,
+    rotation,
+    setRotation,
+  } = props;
+
+  function handlePosXChange(e) {
+    setPosX(e.target.value);
+  }
+
+  function handlePosYChange(e) {
+    setPosY(e.target.value);
+  }
+
+  function handleRotationChange(e) {
+    setRotation(e.target.value);
+  }
+
+
+  // if (!activeElement) {
+  //   return (
+  //     <div className="media-control">
+  //       <div className="media-control-header">
+  //         <p>Media Control</p>
+  //       </div>
+
+  //       <div className="media-control-body">
+  //         <p>Select a media card to adjust its properties</p>
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+  return (
+    <div className="media-control">
+      <div className="media-control-header">
+        {/* <p>{name}</p> */}
+      </div>
+
+      <div className="media-control-body">
+        <div className="media-control-body-pos">
+          <p>Pos-X:</p>
+          <input type="number" value={posX} className="number-input" onChange={handlePosXChange} />
+          <p>Pos-Y:</p>
+          <input type="number" value={posY} className="number-input" onChange={handlePosYChange} />
+        </div>
+
+        <div className="media-control-body-rotate">
+          <p>Rotation:</p>
+          <input type="range" min="-180" max="180" value={rotation} id="rotate-slider" onChange={handleRotationChange} />
+        </div>
+
+        {/* <div className="media-control-body-scale">
+          <p>Scale:</p>
+          <input type="range" min="1" max="200" value={element.scale * 100 || "100"} id="scale-slider" />
+        </div>
+
+        <div className="media-control-body-opacity">
+          <p>Opacity:</p>
+          <input type="range" min="0" max="100" value={element.opacity || "100"} id="opacity-slider" />
+        </div>
+
+        <div className="media-control-body-zindex">
+          <p>Z-Index:</p>
+          <input type="number" value={element.zindex || "0"} className="number-input" />
+        </div> */}
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default MediaControls;
