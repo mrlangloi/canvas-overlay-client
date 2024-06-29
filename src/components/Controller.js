63c03707edbf0f4acceb55798a3837a1cc3f524f
rCheung <rcheung1997@gmail.com>
2024-06-29 05:53:45 -0700

    connected components

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import MediaControl from './MediaControl';
+import React, { useState } from 'react';
+import Layers from './Layers';
+import MediaCard from './MediaCard';
+import MediaControls from './MediaControls';
 
 // sidebar to adjust the properties of current media card
 
-function Controller(media) {
+function Controller() {
+
+  const defaultState = {
+    id: 0,
+    name: "Element",
+    text: "placeholder text",
+    src: "https://via.placeholder.com/150",
+    posX: 0,
+    posY: 0,
+    rotate: 0,
+    scale: 1,
+    opacity: 100,
+    zindex: 0
+  }
+
+  const [posX, setPosX] = useState(0)
+  const [posY, setPosY] = useState(0)
+  const [rotation, setRotation] = useState(0)
+
+  const [activeElement, setActiveElement] = useState(null)
+
+
   return (
     <div className="controller">
-      <MediaControl />
+      <h1>Main Control</h1>
+
+      <Layers 
+        setActiveElement={setActiveElement} 
+      />
+
+      <MediaControls 
+        activeElement={activeElement}
+        posX={posX}
+        setPosX={setPosX}
+        posY={posY}
+        setPosY={setPosY}
+        rotation={rotation}
+        setRotation={setRotation}
+      />
+
+      <MediaCard 
+        id={1} 
+        setActiveElement={setActiveElement} 
+        setPosX={setPosX} 
+        setPosY={setPosY} 
+        rotation={rotation}
+      />
+
+      <MediaCard 
+        id={2} 
+        setActiveElement={setActiveElement} 
+        setPosX={setPosX} 
+        setPosY={setPosY} 
+        rotation={rotation}
+      />
+
     </div>
   )
 }
