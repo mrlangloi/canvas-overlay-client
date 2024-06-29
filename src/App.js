acbbb2b9e3db3cc05fde3f69a3ca500bb3ca7c27
rCheung <rcheung1997@gmail.com>
2024-06-29 05:53:14 -0700

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import Controller from './components/Controller';
-import MediaCard from './components/MediaCard';
 
 function App() {
 
-
-
   return (
     <div className="App">
-      <Controller />
-      <MediaCard id="1"/>
-      <MediaCard id="2"/>
-      <MediaCard id="3"/>
+      <div className="container">
+        <Controller />
+      </div>
     </div>
   );
 }
