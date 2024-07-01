c5eb5ba26c8e23c258f6c99400e32aa938e910ab
rCheung <rcheung1997@gmail.com>
2024-07-01 04:27:32 -0700

    included more controls

--- src/components/MediaControls.js
+++ src/components/MediaControls.js
@@ -19,12 +19,19 @@ function MediaControls(props) {
     setZIndex,
     imgSrc,
     setImgSrc,
+    text,
+    setText
   } = props;
 
   function handleImageSourceChange(e) {
     setImgSrc(e.target.value);
     const imgElement = activeElement.querySelector('img');
     imgElement.src = e.target.value;
+  }
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+    activeElement.querySelector('p').textContent = e.target.value;
   }
 
   function handlePosXChange(e) {
@@ -57,10 +64,30 @@ function MediaControls(props) {
     activeElement.style.zIndex = e.target.value;
   }
 
+  function handleReset(e) {
+    setPosX(0);
+    setPosY(0);
+    setRotation(0);
+    setScale(100);
+    setOpacity(100);
+    setZIndex(0);
+    activeElement.style.top = `100px`;
+    activeElement.style.left = `400px`;
+    activeElement.style.rotate = `0deg`;
+    activeElement.style.scale = `1`;
+    activeElement.style.opacity = `1`;
+    activeElement.style.zIndex = `0`;
+  }
+
+  function handleDelete(e) {
+    // I'll work on this another time
+    // activeElement.remove();
+  }
+
   if (!activeElement) {
     return (
       <div className="media-control">
-        <div className="media-control-header">
+        <div className="media-control-header flex-column">
           <p>Media Control</p>
         </div>
 
@@ -73,48 +100,55 @@ function MediaControls(props) {
 
   return (
     <div className="media-control">
-      <div className="media-control-header">
+      <div className="media-control-header flex-column">
         <p>{activeElement.id}</p>
       </div>
 
-      <div className="media-control-body">
+      <div className="media-control-body flex-column">
         <div className="imageSource">
           <input type="text" className="text-input" id="image-source-input" value={imgSrc} onChange={handleImageSourceChange} />
         </div>
 
-        <div className="position">
+        <div>
+          <input type="text" id="inner-text-input" value={text} onChange={handleTextChange} />
+        </div>
+
+        <div className="position flex-row">
           <p>Pos-X:</p>
           <input type="number" className="number-input" value={posX} onChange={handlePosXChange} />
           <p>Pos-Y:</p>
           <input type="number" className="number-input" value={posY} onChange={handlePosYChange} />
         </div>
 
-        <div className="rotate">
+        <div className="rotate flex-row">
           <p>Rotation:</p>
-          <input type="range" className="range-slider" id="rotate-slider" min="-180" max="180" value={rotation} onChange={handleRotationChange} />
           <input type="number" className="number-input" id="rotate-input" value={rotation} onChange={handleRotationChange} />
+          <input type="range" className="range-slider" id="rotate-slider" min="-180" max="180" value={rotation} onChange={handleRotationChange} />
+          
         </div>
 
-        <div className="scale">
+        <div className="scale flex-row">
           <p>Scale:</p>
-          <input type="range" className="range-slider" id="scale-slider" min="5" max="200" value={scale} onChange={handleScaleChange} />
           <input type="number" className="number-input" id="scale-input" value={scale} onChange={handleScaleChange} />
+          <input type="range" className="range-slider" id="scale-slider" min="5" max="200" value={scale} onChange={handleScaleChange} />
         </div>
 
-        <div className="opacity">
+        <div className="opacity flex-row">
           <p>Opacity:</p>
-          <input type="range" className="range-slider" id="opacity-slider" min="0" max="100" value={opacity} onChange={handleOpacityChange} />
           <input type="number" className="number-input" id="opacity-input" value={opacity} onChange={handleOpacityChange} />
+          <input type="range" className="range-slider" id="opacity-slider" min="0" max="100" value={opacity} onChange={handleOpacityChange} />
         </div>
 
-        <div className="zindex">
+        <div className="zindex flex-row">
           <p>Z-Index:</p>
           <input type="number" className="number-input" value={zIndex} onChange={handleZIndexChange} />
         </div>
 
+        <div className="flex-row">
+          <button className="button" id="reset-button" onMouseUp={handleReset}>Reset</button>
+          <button className="button" id="delete-button" onMouseUp={handleDelete}>Delete</button>
+        </div>
       </div>
-
-
     </div>
   )
 }
