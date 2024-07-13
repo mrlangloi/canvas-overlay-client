ef78815ac8d370499217d46ca4c53844f60f4e6b
rCheung <rcheung1997@gmail.com>
2024-07-13 00:53:28 -0700

    swapped to using usestate

--- src/contexts/CardListContext.js
+++ src/contexts/CardListContext.js
@@ -1,27 +1,13 @@
-import React, { createContext, useRef } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const CardListContext = createContext();
 
 export function CardListContextProvider({children}) {
 
-  const listOfCards = useRef([])
-
-  function addToList(element) {
-    if (element && !listOfCards.current.includes(element)) {
-
-      listOfCards.current.push(element)
-    }
-  }
-
-  function removeFromList(element) {
-    if (element && listOfCards.current.includes(element)) {
-      const index = listOfCards.current.indexOf(element)
-      listOfCards.current.splice(index, 1)
-    }
-  }
+  const [listOfCards, setListOfCards] = useState([])
 
   return (
-    <CardListContext.Provider value={{listOfCards, addToList}}>
+    <CardListContext.Provider value={{listOfCards, setListOfCards}}>
       {children}
     </CardListContext.Provider>
   );
