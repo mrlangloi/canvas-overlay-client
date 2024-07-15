2ff27199b33faeb8aeb9768233fce74ad46477ea
rCheung <rcheung1997@gmail.com>
2024-07-15 12:00:46 -0700

    included title attribute

--- src/components/TwitchEmbed.js
+++ src/components/TwitchEmbed.js
@@ -2,6 +2,7 @@ function TwitchEmbed() {
   return (
     <div id="twitch-embed" style={{ zIndex: -1, }}>
       <iframe src="https://player.twitch.tv/?channel=dearbun&parent=localhost" 
+        title="Twitch Embed"
         frameborder="1" 
         allowfullscreen="false" 
         height="720" 
