b249337aa6e0342c382fe8a2c0e03b4d795b4600
rCheung <rcheung1997@gmail.com>
2024-07-18 14:25:02 -0700

    moved buttons to layers

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,8 +1,4 @@
-import React, { useContext } from 'react';
-import { CardContext } from '../contexts/CardContext';
-import { CardListContext } from '../contexts/CardListContext';
-import { SocketContext } from '../contexts/SocketContext';
-import MediaObj from '../models/MediaObj';
+import React from 'react';
 import Layers from './Layers';
 import MediaControls from './MediaControls';
 
@@ -10,51 +6,9 @@ import MediaControls from './MediaControls';
 
 function Controller() {
 
-  const { listOfCards, setListOfCards, listOfIds, setListOfIds } = useContext(CardListContext)
-  const { activeElement, setActiveElement } = useContext(CardContext)
-  const { emitEvent } = useContext(SocketContext)
-
-  function handleCreateCard() {
-    // if listOfIds contain a false value, then create a new card using its index as the id
-    const index = listOfIds.indexOf(false)
-    if (index !== -1) {
-      setListOfIds(prevList => {
-        const newList = [...prevList]
-        newList[index] = true
-        return newList
-      })
-      const newCard = new MediaObj(index)
-      setListOfCards([...listOfCards, newCard])
-      setActiveElement(newCard)
-      emitEvent('createCard', newCard)
-    }
-  }
-
-  function handleDelete() {
-
-    if (listOfCards.length === 0 || !activeElement) return;
-
-    // remove the card from the list of cards and update listOfIds
-    const id = activeElement.id
-    setListOfIds(prevList => {
-      const newList = [...prevList]
-      newList[id] = false
-      return newList
-    })
-    const newListOfCards = listOfCards.filter(element => element.id !== id)
-    setListOfCards(newListOfCards)
-    setActiveElement(newListOfCards.length > 0 ? newListOfCards[newListOfCards.length - 1] : null)
-    emitEvent('deleteCard', id)
-  }
-
   return (
     <div className="controller">
       <h1>Main Control</h1>
-
-      <div className="flex-row">
-        <button className="button" onMouseUp={handleCreateCard}>+ Create</button>
-        <button className="button" onMouseUp={handleDelete}>- Delete</button>
-      </div>
 
       <Layers />
 
--- src/components/Layers.js
+++ src/components/Layers.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import { CardContext } from '../contexts/CardContext';
 import { CardListContext } from '../contexts/CardListContext';
+import { SocketContext } from '../contexts/SocketContext';
+import MediaObj from '../models/MediaObj';
 import Layer from './Layer';
 
 function Layers(props) {
 
-  const { setActiveElement } = useContext(CardContext);
-  const { listOfCards } = useContext(CardListContext);
+  const { listOfCards, setListOfCards, listOfIds, setListOfIds } = useContext(CardListContext)
+  const { activeElement, setActiveElement } = useContext(CardContext)
+  const { emitEvent } = useContext(SocketContext)
 
   if (listOfCards.length === 0) {
     return (
@@ -19,9 +22,42 @@ function Layers(props) {
     )
   }
 
+  function handleCreateCard() {
+    // if listOfIds contain a false value, then create a new card using its index as the id
+    const index = listOfIds.indexOf(false)
+    if (index !== -1) {
+      setListOfIds(prevList => {
+        const newList = [...prevList]
+        newList[index] = true
+        return newList
+      })
+      const newCard = new MediaObj(index)
+      setListOfCards([...listOfCards, newCard])
+      setActiveElement(newCard)
+      emitEvent('createCard', newCard)
+    }
+  }
+
+  function handleDelete() {
+
+    if (listOfCards.length === 0 || !activeElement) return;
+
+    // remove the card from the list of cards and update listOfIds
+    const id = activeElement.id
+    setListOfIds(prevList => {
+      const newList = [...prevList]
+      newList[id] = false
+      return newList
+    })
+    const newListOfCards = listOfCards.filter(element => element.id !== id)
+    setListOfCards(newListOfCards)
+    setActiveElement(newListOfCards.length > 0 ? newListOfCards[newListOfCards.length - 1] : null)
+    emitEvent('deleteCard', id)
+  }
+
   const layers = listOfCards.map((element, index) => {
     return (
-      <Layer 
+      <Layer
         key={index}
         setActiveElement={setActiveElement}
         element={element}
@@ -31,9 +67,17 @@ function Layers(props) {
 
   return (
     <div className="layers">
-      <p>Layers</p>
+
+      <div className="layers-header">
+        <p>Layers</p>
+        <div className="flex-row">
+          <i className="fa fa-plus" id="create-card-button" onClick={handleCreateCard} />
+          <i className="fa fa-trash" id="delete-card-button" onClick={handleDelete} />
+        </div>
+      </div>
+
       <div className="layers-body">
-          {layers}
+        {layers}
       </div>
     </div>
   )
