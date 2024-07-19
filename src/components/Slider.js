55ed262104c3a8f9ee62fe2774d43636817ba5ce
rCheung <rcheung1997@gmail.com>
2024-07-19 03:41:02 -0700

    fixed values for number fields

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,7 +5,7 @@ function Slider(props) {
   return (
     <div className="rotate flex-row">
       <p>{name}:</p>
-      <input type="number" className="number-input" id="rotate-input" value={value} onChange={handleFunction} />
+      <input type="number" className="number-input" id="rotate-input" min={minValue} max={maxValue} value={value} onChange={handleFunction} />
       <input type="range" className="range-slider" id="rotate-slider" min={minValue} max={maxValue} value={value} onChange={handleFunction} />
     </div>
   )
