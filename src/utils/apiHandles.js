4ba4e897178acae4474592d8489317b5b3dda7e9
rCheung <rcheung1997@gmail.com>
2024-07-24 07:12:20 -0700

    initialized connections to mongodb

---
+++ src/utils/apiHandles.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+// get all media cards
+export async function getMediaCards() {
+  try {
+    const response = await axios.get(`${API_URL}`)
+    return response.data
+  } 
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// create a new media card
+export async function createMediaCard(card) {
+  try {
+    const response = await axios.post(`${API_URL}`, card)
+    return response.data
+  } 
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// update a media card
+export async function updateMediaCard(card) {
+  try {
+    const response = await axios.put(`${API_URL}/${card.id}`, card)
+    return response.data
+  } 
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// delete a media card
+export async function deleteMediaCard(id) {
+  try {
+    const response = await axios.delete(`${API_URL}/${id}`)
+    return response.data
+  } 
+  catch (error) {
+    console.error(error)
+  }
+}
