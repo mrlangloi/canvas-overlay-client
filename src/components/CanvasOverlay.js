57dc4b02b38800951746cc73e08087af7752326e
rCheung <rcheung1997@gmail.com>
2024-07-24 07:07:10 -0700

    removed test useEffect

--- src/components/CanvasOverlay.js
+++ src/components/CanvasOverlay.js
@@ -1,27 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { CardListContext } from '../contexts/CardListContext';
-import MediaObj from '../models/MediaObj';
 import MediaCard from './MediaCard';
 import TwitchEmbed from './TwitchEmbed';
 
 function CanvasOverlay() {
 
-  const { listOfCards, setListOfCards, listOfIds, setListOfIds } = useContext(CardListContext);
+  const { listOfCards } = useContext(CardListContext);
 
-  useEffect(() => {
-    // create 3 media cards when the app first loads to test the functionality
-    if (listOfCards.length === 0) {
-      setListOfIds(prevList => {
-        const newList = [...prevList];
-        newList[0] = newList[1] = newList[2] = true;
-        return newList;
-      })
-      setListOfCards([new MediaObj(0), new MediaObj(1), new MediaObj(2)])
-    }
-  }, [])
-  
-
-  const cards = listOfCards.map((element, index) => {
+  const cards = listOfCards?.map((element, index) => {
     return (
       <MediaCard
         key={index}
