3cf3acef158bedddecc5632ada10077a0f3f914a
rCheung <rcheung1997@gmail.com>
2024-07-24 07:09:53 -0700

    included connections to mongodb

--- src/components/Layers.js
+++ src/components/Layers.js
@@ -3,6 +3,7 @@ import { CardContext } from '../contexts/CardContext';
 import { CardListContext } from '../contexts/CardListContext';
 import { SocketContext } from '../contexts/SocketContext';
 import MediaObj from '../models/MediaObj';
+import { createMediaCard, deleteMediaCard } from '../utils/apiHandles';
 import Layer from './Layer';
 
 function Layers(props) {
@@ -11,37 +12,51 @@ function Layers(props) {
   const { activeElement, setActiveElement } = useContext(CardContext)
   const { emitEvent } = useContext(SocketContext)
 
-  function handleCreateCard() {
-    // if listOfIds contain a false value, then create a new card using its index as the id
-    const index = listOfIds.indexOf(false)
-    if (index !== -1) {
-      setListOfIds(prevList => {
-        const newList = [...prevList]
-        newList[index] = true
-        return newList
-      })
-      const newCard = new MediaObj(index)
-      setListOfCards([...listOfCards, newCard])
-      setActiveElement(newCard)
-      emitEvent('createCard', newCard)
+  async function handleCreateCard() {
+    try {
+      // if listOfIds contain a false value, then create a new card using its index as the id
+      const index = listOfIds.indexOf(false)
+      if (index !== -1) {
+
+        setListOfIds(prevList => {
+          const newList = [...prevList]
+          newList[index] = true
+          return newList
+        })
+
+        const newCard = new MediaObj(index)
+        setListOfCards([...listOfCards, newCard])
+        setActiveElement(newCard)
+        emitEvent('createCard', newCard)
+        await createMediaCard(newCard)
+      }
+    }
+    catch (error) {
+      console.log(error)
     }
   }
 
-  function handleDelete() {
+  async function handleDelete() {
+    try {
 
-    if (listOfCards.length === 0 || !activeElement) return;
+      if (listOfCards.length === 0 || !activeElement) return;
 
-    // remove the card from the list of cards and update listOfIds
-    const id = activeElement.id
-    setListOfIds(prevList => {
-      const newList = [...prevList]
-      newList[id] = false
-      return newList
-    })
-    const newListOfCards = listOfCards.filter(element => element.id !== id)
-    setListOfCards(newListOfCards)
-    setActiveElement(null)
-    emitEvent('deleteCard', id)
+      // remove the card from the list of cards and update listOfIds
+      const id = activeElement.id
+      setListOfIds(prevList => {
+        const newList = [...prevList]
+        newList[id] = false
+        return newList
+      })
+      const newListOfCards = listOfCards.filter(element => element.id !== id)
+      setListOfCards(newListOfCards)
+      setActiveElement(null)
+      emitEvent('deleteCard', id)
+      await deleteMediaCard(id)
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
 
   const layers = listOfCards.map((element, index) => {
