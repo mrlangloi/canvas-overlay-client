76015e45936eb91bf4c565302510c22b47912002
rCheung <rcheung1997@gmail.com>
2024-07-30 14:00:11 -0700

    created file

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
+
+export const UserContext = createContext()
+
+export const UserContextProvider = ({ children }) => {
+
+  const [user, setUser] = useState(null)
+  const [authorized, setAuthorized] = useState(false)
+
+  useEffect(() => {
+
+    async function getUser() {
+      try {
+        const response = await axios.get('http://localhost:3500/auth/user', { withCredentials: true })
+
+        console.log(response.data)
+
+        if (response.data) {
+          setUser(response.data)
+          if (response.status === 200) {
+            setAuthorized(true)
+          }
+          else {
+            setAuthorized(false)
+          }
+        }
+        else {
+          setUser(null)
+          setAuthorized(false)
+        }
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    getUser();
+
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ user, authorized }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
