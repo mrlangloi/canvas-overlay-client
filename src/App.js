440dece63348a7ecad2226e80596f0398d3be091
rCheung <rcheung1997@gmail.com>
2024-07-30 13:59:27 -0700

    moved combinedprovider context to index.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.css';
 import CanvasOverlay from './components/CanvasOverlay';
 import Controller from './components/Controller';
-import CombinedProvider from './contexts/CombinedProvider';
 
 function App() {
 
   return (
     <div className="App">
       <div className="container">
-        <CombinedProvider>
           <Controller />
           <CanvasOverlay />
-        </CombinedProvider>
       </div>
     </div>
   );
