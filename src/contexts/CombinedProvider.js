e4ef1e052d9a59e5aa12778e0b210ce5be41f1d1
rCheung <rcheung1997@gmail.com>
2024-07-30 14:00:26 -0700

    updated with UserContextProvider

--- src/contexts/CombinedProvider.js
+++ src/contexts/CombinedProvider.js
@@ -3,20 +3,23 @@ import { CardContextProvider } from './CardContext';
 import { CardListContextProvider } from './CardListContext';
 import { SocketContextProvider } from './SocketContext';
 import { StreamContextProvider } from './StreamContext';
+import { UserContextProvider } from './UserContext';
 
 // combines all the contexts into one provider
 
 const CombinedProvider = ({ children }) => {
   return (
-    <SocketContextProvider>
-      <CardListContextProvider>
-        <CardContextProvider>
-          <StreamContextProvider>
-            {children}
-          </StreamContextProvider>
-        </CardContextProvider>
-      </CardListContextProvider>
-    </SocketContextProvider>
+    <UserContextProvider>
+      <SocketContextProvider>
+        <CardListContextProvider>
+          <CardContextProvider>
+            <StreamContextProvider>
+              {children}
+            </StreamContextProvider>
+          </CardContextProvider>
+        </CardListContextProvider>
+      </SocketContextProvider>
+    </UserContextProvider>
   );
 }
 
