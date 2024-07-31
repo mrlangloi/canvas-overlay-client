da93acf61e36d3e6de76d17f38a7b0f376bcc039
rCheung <rcheung1997@gmail.com>
2024-07-31 00:34:45 -0700

    fixed bug

--- src/components/TwitchLogin.js
+++ src/components/TwitchLogin.js
@@ -7,7 +7,7 @@ function TwitchLogin() {
   const { user, setUser } = useContext(UserContext);
 
   async function handleLogin(e) {
-    await axios.get(`${process.env.REACT_APP_API_URL}/auth/twitch`, { withCredentials: true })
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/twitch`, { withCredentials: true })
       .then(response => {
         console.log(response.data);
       })
