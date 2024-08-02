c9fc39def15c3d21d50da9c9c22ea80ef1a15acd
rCheung <rcheung1997@gmail.com>
2024-08-01 17:20:36 -0700

    merged with CardContext

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -1,30 +1,51 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { getMediaCards } from '../utils/apiHandles';
+import { SocketContext } from './SocketContext';
 
-// stores the active card element and its properties
+// stores the list of cards and their available ids
 
 export const CardContext = createContext();
 
-export function CardContextProvider({children}) {
+export function CardContextProvider({ children }) {
+
+  const { socket } = useContext(SocketContext);
+
+  const [listOfIds, setListOfIds] = useState(new Array(50).fill(false));
+  const [listOfCards, setListOfCards] = useState([]);
 
   const [activeElement, setActiveElement] = useState(null);
-
   const [cardState, setCardState] = useState({
     name: "",
     visibility: "hidden",
     src: "",
     text: "",
     fontSize: "16",
-    posX: "0",
-    posY: "0",
     width: "-1",
     height: "-1",
     rotation: "0",
     opacity: "100",
     zIndex: "0",
   });
+  const [cardPosition, setCardPosition] = useState({ 
+    posX: "0", 
+    posY: "0" 
+  }); // for the draggable card feature in MediaCard
+
+  const values = {
+    listOfCards,
+    setListOfCards,
+    listOfIds,
+    setListOfIds,
+    activeElement,
+    setActiveElement,
+    cardState,
+    setCardState,
+    cardPosition,
+    setCardPosition,
+  };
 
   useEffect(() => {
-    if (!activeElement) return
+    if (!activeElement) return;
 
     setCardState({
       name: activeElement.name,
@@ -32,8 +53,6 @@ export function CardContextProvider({children}) {
       src: activeElement.src,
       text: activeElement.text,
       fontSize: activeElement.fontSize,
-      posX: activeElement.posX,
-      posY: activeElement.posY,
       width: activeElement.width,
       height: activeElement.height,
       rotation: activeElement.rotation,
@@ -43,10 +62,87 @@ export function CardContextProvider({children}) {
       zIndex: activeElement.zIndex,
     });
 
+    setCardPosition({
+      posX: activeElement.posX,
+      posY: activeElement.posY,
+    });
+
   }, [activeElement, setCardState])
 
+  // updates the card with the new data
+  // since there are too many variables to update, we just replace the card instead
+  const updateCard = (card) => {
+    setListOfCards((prev) => {
+      const cardIndex = prev.findIndex((element) => element.id === card.id);
+      if (cardIndex !== -1) {
+        const updatedCards = [...prev];
+        updatedCards[cardIndex] = card;
+        return updatedCards;
+      };
+      return prev;
+    })
+  };
+
+  // creates a new card
+  const createCard = (card) => {
+    setListOfCards((prev) => [...prev, card]);
+    setListOfIds((prevId) => {
+      const updatedIds = [...prevId];
+      updatedIds[card.id] = true;
+      return updatedIds;
+    });
+  };
+
+  // deletes a card
+  const deleteCard = (id) => {
+    setListOfCards((prev) => prev.filter((element) => element.id !== id));
+    setListOfIds((prevId) => {
+      const updatedIds = [...prevId];
+      updatedIds[id] = false;
+      return updatedIds;
+    });
+  };
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('updateCard', updateCard);
+      socket.on('createCard', createCard);
+      socket.on('deleteCard', deleteCard);
+
+      return () => {
+        if (socket) {
+          socket.off('updateCard', updateCard);
+          socket.off('createCard', createCard);
+          socket.off('deleteCard', deleteCard);
+        }
+      };
+    }
+  }, [socket]);
+
+
+  // on mount, fetch the media cards and update the list of cards and ids
+  useEffect(() => {
+    async function fetchMediaCards() {
+      try {
+        const cards = await getMediaCards();
+        setListOfCards(cards);
+        setListOfIds((prev) => {
+          const updatedIds = [...prev];
+          cards.forEach((card) => {
+            updatedIds[card.id] = true;
+          });
+          return updatedIds;
+        });
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+    fetchMediaCards();
+  }, []);
+
   return (
-    <CardContext.Provider value={{ activeElement, setActiveElement, cardState, setCardState }}>
+    <CardContext.Provider value={ values }>
       {children}
     </CardContext.Provider>
   );
