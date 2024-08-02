56d9c06d626e1a82c3a7304b36d8340d49b240e8
rCheung <rcheung1997@gmail.com>
2024-08-01 17:24:29 -0700

    included a ref

--- src/components/MouseDisplay.js
+++ src/components/MouseDisplay.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { SocketContext } from '../contexts/SocketContext'
 import { UserContext } from '../contexts/UserContext'
 
@@ -7,9 +7,24 @@ function MouseDisplay() {
   const { positions } = useContext(SocketContext)
   const { user } = useContext(UserContext)
 
-  const renderedPointers = Object.entries(positions).map(([id, pos]) => (
+  const mouseRef = useRef()
+
+  useEffect(() => {
+    if (!user) return
+
+    const mouse = mouseRef.current
+    mouse.style.display = 'none'
+
+    return () => {
+      mouse.style.display = 'block'
+    }
+  })
+
+  return (
+    <>
       <i
         key={id}
+        ref={mouseRef}
         className='fa fa-mouse-pointer'
         style={{
           position: 'absolute',
@@ -17,16 +32,11 @@ function MouseDisplay() {
           left: pos.x,
           fontSize: '12px',
           color: 'white',
-          zIndex: 999,
+          zIndex: 1001,
         }}
       >
         <p style={{ fontSize: '10px' }}>{user.display_name}</p>
       </i>
-    ))
-
-  return (
-    <>
-      {renderedPointers}
     </>
   )
 }
